ed1cb305a1e3e0ed2f63b9d5fc63d67fce890168
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-25 21:42:52 +0700

    Operator di java sama kaya di cpp

---
+++ Operator.java
@@ -0,0 +1,26 @@
+public class kalkulator {
+	public static void main(String[] args) {
+		int a =20;
+		int b =10;
+		int hasil;
+		
+		//penjumlahan 
+		System.out.println(a+b);
+		
+		//pengurangan 
+		hasil = a-b;
+		System.out.println(hasil);
+		
+		//perkalian 
+		hasil = a*b;
+		System.out.printf("%d × %d = %d",a,b,hasil);
+		
+		// pembagian 
+		System.out.println ();
+		System.out.println(a/b);
+		
+		//sisa bagi
+		System.out.println (a%b);
+		
+	}
+}
