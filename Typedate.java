533e17b469a02ce60361d8221d475a59ff740799
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-25 21:44:23 +0700

    Tipe data pada java sama kaya cpp

---
+++ Typedate.java
@@ -0,0 +1,20 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		// sama kaya cpp
+		long a = 133;
+		short sh = 466;
+		int i = 77;
+		char c = 'A';
+		float f = 785;
+		double d = 789.9;
+
+
+		System.out.println(a);
+		System.out.println(sh);
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(d);
+	}
+}
