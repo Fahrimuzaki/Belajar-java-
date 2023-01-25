3ed2cef99edc2a09a56fafeabc2c9916a8dfa56c
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-25 21:40:19 +0700

    Belajar output java

---
+++ Output.java
@@ -0,0 +1,15 @@
+public class hello_word {
+	
+	public static void main(String[] args)
+	 { 
+	 //macm macam print
+	 // 1enter
+	System.out.println("l am the king of progemer");
+	
+	// 2 tanpa enter
+	System.out.print("saya gabut");
+	
+	// 3 output angka
+	System.out.printf ("\nNomer %d",999);
+	  }
+}
