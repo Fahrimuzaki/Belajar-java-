3087d3da4c02ee38aa552c5b332d08e9d71ce81b
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-26 21:46:43 +0700

    Create Operator_perbandingan.java

---
+++ Operator_perbandingan.java
@@ -0,0 +1,26 @@
+public class Main{
+	public static void main(String[] args) {
+		int a = 5 ;
+		int b = 5;
+		
+		// operator komparasi adalah operator perbandingan 
+		boolean c1 = (a == b);
+		boolean c2 =(a != b);
+		/*
+		  ini operator nya 
+		  
+		  1.  ==
+		  2.  !=
+		  3.  <=
+		  4.  >=
+		  
+		  5.  >
+		  6.  <
+		
+		
+		*/
+		
+		System.out.println(c1);
+		System.out.println(c2);
+	}
+}
