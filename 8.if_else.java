3b6e3e85495f9ed86fb78b76638d4ab09718b6bf
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-29 02:06:34 +0700

    Create 8.if_else.java

---
+++ 8.if_else.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner nama = new Scanner(System.in);
+        
+        System.out.print("Masukan angka : ");
+        int date = nama.nextInt();
+        
+        
+        
+        
+        
+        //   Materi if else sama kaya di cpp
+        
+        if(date == 5){
+            System.out.print("if terpakai");
+        }
+        else if(date == 10){
+            
+            System.out.println("else if terpakai");
+        }else{
+            
+            System.out.println("else terpakai");
+        }
+        
+        
+        
+    }
+}
