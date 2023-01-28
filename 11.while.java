cecb3d8685e0b95fdee9728bd61d19383d7df8f8
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-29 03:40:09 +0700

    Create 11.while.java

---
+++ 11.while.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		// Materi while ini sama kaya cpp
+		
+		int a = 1;  // awal
+		while(a <= 3) // akhir
+		{
+		    System.out.println("Hello ");
+		    a++; // proses
+		}
+	}
+}
