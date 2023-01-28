c49c61bf3cf28d71ffb82b07b9853585ee048d63
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-29 03:55:15 +0700

    Create 12.do_while.java

---
+++ 12.do_while.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+    
+          // Materi ini sama kaya cpp
+    int a = 1; //awal
+    
+    do{
+          
+          System.out.println("Hello mr.fakhri ");
+        a++; //proses 
+        
+    }while(a <=10);  //akhir
+    
+    
+   }
+}
