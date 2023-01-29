97910fdc71d19216fe31673151592f4c1c0e7d52
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-01-29 08:38:51 +0700

    Create 13.for.java

---
+++ 13.for.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("awal program ");
+		
+		
+		
+  // Materi ini sama kaya cpp
+  
+		//  for(awal;  akhir;  proses){  }
+		
+		for(int a = 1; a <= 10; a++){
+		    		System.out.println("awal program ");
+		}
+	}
+}
