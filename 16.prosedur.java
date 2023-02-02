41de233fcb310ac14349c68be8a432854fc68b03
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-02-02 20:53:56 +0700

    Create 16.prosedur.java

---
+++ 16.prosedur.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		ko("hai");
+	}
+	 
+	 // cara buat prosedur di java  sama kaya
+	 // di cpp, beda nya ada static kalau di java
+	public static void ko(String hasil){
+	    System.out.print(hasil);
+	}
+}
