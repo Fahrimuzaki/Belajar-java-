28be0968b801119bc7dddfe60c29d9e3f8f28084
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-02-02 20:55:59 +0700

    Create 18.overload.java

---
+++ 18.overload.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		Sys
+	}
+	
+	
+	static int tambah(int a){
+	    int hasil = a + a;
+	    return hasil;
+	}
+	  // ini adalah fungsi overload
+	static float tambah(float a){
+	    float hasil = a + a;
+	    return hasil;
+	}
+
+	
+}
