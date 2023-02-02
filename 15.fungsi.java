1952960a8ea418a789d32e2c38575b7e1c6861fd
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-02-02 20:53:00 +0700

    Create 15.fungsi.java

---
+++ 15.fungsi.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		int date = mtk(10,10);
+		System.out.print(date);
+		
+	}
+		
+		// cara buat function di java hampir sama
+		// kaya cpp ,bedanya di sini ada static 
+		
+        	private  static int mtk(int a,int b){
+		    int hasil ;
+		    hasil = a + b; 
+		    return hasil;
+		    
+		}
+}
