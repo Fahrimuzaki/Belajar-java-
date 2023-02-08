bf9a9c14b8bb28a7034ebe724521d6703ce1cdd0
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-02-08 07:01:35 +0700

    Create 19(a).array.java

---
+++ 19(a).array.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		// Materi array pada java
+
+// Cara buat
+		  // 1. Membuat langsung 
+		   //tipe_data [] nama = {value};
+		      int nama[] = {0,1,2};
+		      
+		  //2. Deklarasi 
+		     //tipe_data [] nama = new tipe_data[jumblah array];
+		     
+		     int [] nama2 = new int[5];
+		     
+		     
+		     
+// Cara panggil 
+    //sama kaya di cpp yaitu :
+    
+  //  nama[index];
+  System.out.print(   nama[2]  + "\n");
+  
+  
+  
+  
+  //Di luar materi
+  
+  System.out.print(Arrays.toString(nama));
+	}
+}
