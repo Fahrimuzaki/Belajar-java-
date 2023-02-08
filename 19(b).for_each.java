67ea45c843f21182e7016f6094db5c09ef65a974
M.FAKHRI.M <121482906+Fahrimuzaki@users.noreply.github.com>
2023-02-08 07:02:45 +0700

    Create  19(b).for_each.java

---
+++ 19(b).for_each.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		//Materi for each array
+		
+		int nama[] = {0,1,2,3,4,5,6,7,8,9};
+		
+		//RUMUS 
+	//	for(int nama : array){}
+		
+		for(int angka : nama){
+		    System.out.print("\n isi array nya = " + angka);
+		}
+	}
+}
